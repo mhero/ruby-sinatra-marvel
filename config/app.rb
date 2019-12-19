2835af880d0099d372d678208d4fccdedcf3e21a
mhero <gdmarav374@gmail.com>
2019-12-19 11:07:43 -0500

    Clean path scan to include all paths

--- config/app.rb
+++ config/app.rb
@@ -2,5 +2,4 @@ require "sinatra"
 require "sinatra/base"
 require "dotenv/load"
 
-Dir[File.join("controllers", "**/*_controller.rb")].each { |file| require File.expand_path(file) }
-Dir[File.join("services", "**/*.rb")].each { |file| require File.expand_path(file) }
+Dir[File.join("./", "**/*.rb")].each { |file| require File.expand_path(file) }
