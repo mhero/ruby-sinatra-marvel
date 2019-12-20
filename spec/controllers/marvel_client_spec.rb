814633a9080633ac1b86a3fcfad16783fcd35278
mhero <gdmarav374@gmail.com>
2019-12-20 12:38:52 -0500

    Add test for story endpoint

--- spec/controllers/marvel_client_spec.rb
+++ spec/controllers/marvel_client_spec.rb
@@ -7,8 +7,19 @@ describe MarvelClient do
     end
   end
 
+  let(:story_response) do
+    VCR.use_cassette("marvel/story", match_requests_on: [:body]) do
+      MarvelClient.new.character_stories("1009351", limit: 8)
+    end
+  end
+
   it "can fetch & parse character data" do
     expect(character_response).to be_kind_of(Character)
     expect(character_response.name).to eq("Hulk")
+  end
+
+  it "can fetch & parse story data" do
+    expect(story_response).to be_kind_of(Array)
+    expect(story_response.count).to eq(8)
   end
 end
