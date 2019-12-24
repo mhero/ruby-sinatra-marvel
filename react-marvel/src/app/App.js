b868e4a56b7e8b850c1be0b9698325ba6a5668c2
mhero <gdmarav374@gmail.com>
2019-12-24 05:06:28 -0500

    Set base html for whole app

--- react-marvel/src/app/App.js
+++ react-marvel/src/app/App.js
@@ -7,19 +7,19 @@ const App = () => {
   const [search, setSearch] = useState(null);
   const [error, setError] = useState(null);
 
-
-  const onClick = () => {
+  const getCharacter = () => {
     axios.get(`${process.env.REACT_APP_API_URL}/character?name=${search}`)
     .then(function (response) {
       let character = response.data.data[0].attributes
       setCharacter(character);
+      setError(null);
     })
     .catch(function (error) {
       // handle error
       if (error.response.status === 404) {
         setError({status: 404, message: "Character not found"});
       } else {
-        setError({status: 500, message: " Error in server, please try again in some minutes"});
+        setError({status: 500, message: "Error in server, please try again in some minutes"});
       }
       setCharacter(null);
     });
@@ -27,25 +27,47 @@ const App = () => {
 
   return (
     <div className="App">
-      <input type="text" id="name" name="name" placeholder="Search a Marvel Character"
-        onChange={(event) => setSearch(event.target.value) }/>
-      <button type="button" onClick={onClick}>Search Character</button>
+      <div class="search">
+        <input type="text" class= "search-input" id="name" name="name" placeholder="Search a Marvel Character"
+          onChange={(event) => setSearch(event.target.value) }/>
+        <button type="button" onClick={getCharacter}>Search Character</button>
+      </div>
 
       {character && (
-        <>
-          <img src= {character.thumbnail} />
-
-          <ul>
-            <li>{character.name}</li>
-            <li>{character.description}</li>
-          </ul>
-        </>
+        <div class="results">
+          <div class="panel character-info">
+            <h1>{character.name}</h1>
+            <img src= {character.thumbnail} />
+            <p>{character.description}</p>
+            <button type="button">See stories</button>
+          </div>
+          <div class="panel stories-info">
+            <h1>Stories</h1>
+            <table>
+              <thead>
+                  <tr>
+                      <th>Title</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>Title 1</tr>
+                  <tr>Title 2</tr>
+                  <tr>Title 3</tr>
+                  <tr>Title 4</tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="panel story-characters-info">
+            <h1>Starring</h1>
+            <p>Story Characters here</p>
+          </div>
+        </div>
       )}
 
       {error && (
-        <>
+        <div class="error-message">
           {error.message}
-        </>
+        </div>
       )}
     </div>
   );
